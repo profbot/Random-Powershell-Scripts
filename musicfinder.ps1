5d2bbc2251c844c66afaac2262c555a9e63771c5
Prof <58375840+profbot@users.noreply.github.com>
2020-10-17 16:08:43 +0100

    Updated dir name.

--- musicfinder.ps1
+++ musicfinder.ps1
@@ -2,14 +2,14 @@ mkdir "MusicInfo"
 
 Set-Location -Path "./MusicInfo"
 # Scans for .flac files
-Get-ChildItem -Path "C:\Users\nbbel\Music" -Filter "*.flac*" -Recurse -File >> flacfiles.txt
-Get-ChildItem -Path "C:\Users\nbbel\deemix Music\" -Filter "*.flac*" -Recurse -File >> flacfiles.txt
+Get-ChildItem -Path "C:\Users\profbot\Music" -Filter "*.flac*" -Recurse -File >> flacfiles.txt
+Get-ChildItem -Path "C:\Users\profbot\deemix Music\" -Filter "*.flac*" -Recurse -File >> flacfiles.txt
 
 # Scans for .mp3 files
-Get-ChildItem -Path "C:\Users\nbbel\Music" -Filter "*.mp3*" -Recurse -File >> mp3files.txt
-Get-ChildItem -Path "C:\Users\nbbel\deemix Music\" -Filter "*.mp3*" -Recurse -File >> mp3files.txt
+Get-ChildItem -Path "C:\Users\profbot\Music" -Filter "*.mp3*" -Recurse -File >> mp3files.txt
+Get-ChildItem -Path "C:\Users\profbot\deemix Music\" -Filter "*.mp3*" -Recurse -File >> mp3files.txt
 
 # Scans for .opus files
-Get-ChildItem -Path "C:\Users\nbbel\Music" -Filter "*.opus*" -Recurse -File >> 'opusfiles.txt'
+Get-ChildItem -Path "C:\Users\profbot\Music" -Filter "*.opus*" -Recurse -File >> 'opusfiles.txt'
 
 
